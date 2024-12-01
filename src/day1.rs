2057c3cf20a1cdec58c9ab7abe808e8def35a4e4
James Skelton <d.j.skelton1@gmail.com>
2024-12-01 15:57:09 +0000

    Optimisations based on input and valgrind

--- src/day1.rs
+++ src/day1.rs
@@ -1,49 +1,60 @@
+/// A solution to day one of AOC 2024.
+///
+/// This code makes optimisations based on the following input observations:
+///     - Each line in the input is of the form r"\d{5}\s{3}\d{5}\n" (i.e.,
+///       this solution WILL NOT work on the example given in the description).
+///     - There are 1,000 entries in the input.
 use rustc_hash::FxHashMap;
 
 struct NumberIter<'a> {
     text: &'a str,
     pos: usize,
+    extra_skip: bool,
 }
 
 impl<'a> Iterator for NumberIter<'a> {
     type Item = i64;
 
     fn next(&mut self) -> Option<Self::Item> {
+        if self.pos + 5 > self.text.len() {
+            return None;
+        }
         let mut n = 0i64;
-        let mut capture = false;
 
-        for idx in self.pos..self.text.len() {
-            let c = self.text.as_bytes()[idx];
-            if c.is_ascii_digit() {
-                capture = true;
-                n *= 10;
-                n += (c - 48) as i64;
-            } else if capture {
-                self.pos = idx + 1;
-                return Some(n);
-            }
+        for c in &self.text.as_bytes()[self.pos..self.pos + 5] {
+            n *= 10;
+            n += (*c - 48) as i64;
         }
 
-        // Check in case file has no newline at end of file.
-        if capture {
-            self.pos = self.text.len();
-            return Some(n);
+        if self.extra_skip {
+            self.pos += 8;
+            self.extra_skip = false;
+        } else {
+            self.pos += 6;
+            self.extra_skip = true;
         }
-        return None;
+        Some(n)
+    }
+}
+
+impl<'a> NumberIter<'a> {
+    fn new(text: &str) -> NumberIter {
+        NumberIter {
+            text,
+            pos: 0,
+            extra_skip: true,
+        }
     }
 }
 
 #[inline(always)]
 fn parse_input_part1(contents: &str) -> (Vec<i64>, Vec<i64>) {
-    let mut list_a: Vec<i64> = Vec::new();
-    let mut list_b: Vec<i64> = Vec::new();
+    let mut list_a: Vec<i64> = Vec::with_capacity(1_000);
+    let mut list_b: Vec<i64> = Vec::with_capacity(1_000);
 
     let mut populate_a = true;
 
-    let number_iter = NumberIter {
-        text: contents,
-        pos: 0,
-    };
+    let number_iter = NumberIter::new(contents);
     for number in number_iter {
         if populate_a {
             list_a.push(number);
@@ -58,15 +69,12 @@ fn parse_input_part1(contents: &str) -> (Vec<i64>, Vec<i64>) {
 
 #[inline(always)]
 fn parse_input_part2(contents: &str) -> (Vec<i64>, FxHashMap<i64, i64>) {
-    let mut list_a: Vec<i64> = Vec::new();
+    let mut list_a: Vec<i64> = Vec::with_capacity(1_000);
     let mut b_count_map = FxHashMap::default();
 
     let mut populate_a = true;
 
-    let number_iter = NumberIter {
-        text: contents,
-        pos: 0,
-    };
+    let number_iter = NumberIter::new(contents);
     for number in number_iter {
         if populate_a {
             list_a.push(number);
@@ -90,7 +98,6 @@ pub fn part1(contents: &str) -> i64 {
 
 pub fn part2(contents: &str) -> i64 {
     let (a, b) = parse_input_part2(contents);
-
     a.iter().map(|i| i * *b.get(i).get_or_insert(&0)).sum()
 }
 
@@ -110,19 +117,5 @@ mod tests {
         let contents = include_str!("../inputs/day01.txt");
         let result = part2(contents);
         assert_eq!(result, 19_437_052);
-    }
-
-    #[test]
-    fn test_part_one_example_solution() {
-        let contents = include_str!("../inputs/day01_example.txt");
-        let result = part1(&contents);
-        assert_eq!(result, 11);
-    }
-
-    #[test]
-    fn test_part_two_example_solution() {
-        let contents = include_str!("../inputs/day01_example.txt");
-        let result = part2(contents);
-        assert_eq!(result, 31);
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let content = include_str!("../inputs/day01.txt");
+    let res1 = aoc_2024::day1::part1(content);
+    let res2 = aoc_2024::day1::part2(content);
+}
