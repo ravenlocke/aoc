5d187a2c69b0a95c3e6cd56206d5a11881f9e597
James Skelton <d.j.skelton1@gmail.com>
2024-12-01 13:25:27 +0000

    Updated to use iterator

--- src/day1.rs
+++ src/day1.rs
@@ -1,33 +1,86 @@
 use rustc_hash::FxHashMap;
 
-fn parse_input(contents: &str) -> (Vec<i64>, Vec<i64>) {
+struct NumberIter<'a> {
+    text: &'a str,
+    pos: usize,
+}
+
+impl<'a> Iterator for NumberIter<'a> {
+    type Item = i64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut n = 0i64;
+        let mut capture = false;
+
+        for idx in self.pos..self.text.len() {
+            let c = self.text.as_bytes()[idx];
+            if c.is_ascii_digit() {
+                capture = true;
+                n *= 10;
+                n += (c - 48) as i64;
+            } else if capture {
+                self.pos = idx + 1;
+                return Some(n);
+            }
+        }
+
+        // Check in case file has no newline at end of file.
+        if capture {
+            self.pos = self.text.len();
+            return Some(n);
+        }
+        return None;
+    }
+}
+
+#[inline(always)]
+fn parse_input_part1(contents: &str) -> (Vec<i64>, Vec<i64>) {
     let mut list_a: Vec<i64> = Vec::new();
     let mut list_b: Vec<i64> = Vec::new();
 
     let mut populate_a = true;
 
-    let mut n: i64 = 0;
-    for char in contents.chars() {
-        if char.is_ascii_digit() {
-            n *= 10;
-            n += (char as i64) - 48
-        } else if n != 0 {
-            if populate_a {
-                list_a.push(n)
-            } else {
-                list_b.push(n);
-            }
-
-            n = 0;
-            populate_a ^= true
+    let number_iter = NumberIter {
+        text: contents,
+        pos: 0,
+    };
+    for number in number_iter {
+        if populate_a {
+            list_a.push(number);
+        } else {
+            list_b.push(number);
         }
+        populate_a ^= true
     }
 
     (list_a, list_b)
 }
 
+#[inline(always)]
+fn parse_input_part2(contents: &str) -> (Vec<i64>, FxHashMap<i64, i64>) {
+    let mut list_a: Vec<i64> = Vec::new();
+    let mut b_count_map = FxHashMap::default();
+
+    let mut populate_a = true;
+
+    let number_iter = NumberIter {
+        text: contents,
+        pos: 0,
+    };
+    for number in number_iter {
+        if populate_a {
+            list_a.push(number);
+        } else {
+            *b_count_map.entry(number).or_default() += 1;
+        }
+        populate_a ^= true
+    }
+
+    (list_a, b_count_map)
+}
+
 pub fn part1(contents: &str) -> i64 {
-    let (mut a, mut b) = parse_input(contents);
+    let (mut a, mut b) = parse_input_part1(contents);
 
     a.sort_unstable();
     b.sort_unstable();
@@ -36,14 +89,9 @@ pub fn part1(contents: &str) -> i64 {
 }
 
 pub fn part2(contents: &str) -> i64 {
-    let (a, b) = parse_input(contents);
-    let map = {
-        let mut m = FxHashMap::default();
-        b.iter().for_each(|i| *m.entry(*i).or_default() += 1);
-        m
-    };
+    let (a, b) = parse_input_part2(contents);
 
-    a.iter().map(|i| i * *map.get(i).get_or_insert(&0)).sum()
+    a.iter().map(|i| i * *b.get(i).get_or_insert(&0)).sum()
 }
 
 #[cfg(test)]
@@ -62,5 +110,19 @@ mod tests {
         let contents = include_str!("../inputs/day01.txt");
         let result = part2(contents);
         assert_eq!(result, 19_437_052);
+    }
+
+    #[test]
+    fn test_part_one_example_solution() {
+        let contents = include_str!("../inputs/day01_example.txt");
+        let result = part1(&contents);
+        assert_eq!(result, 11);
+    }
+
+    #[test]
+    fn test_part_two_example_solution() {
+        let contents = include_str!("../inputs/day01_example.txt");
+        let result = part2(contents);
+        assert_eq!(result, 31);
     }
 }
