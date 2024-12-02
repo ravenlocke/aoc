266d9edaf39697a74b1774826dec9afcb3412496
James Skelton <d.j.skelton1@gmail.com>
2024-12-02 13:10:19 +0000

    Initial solution for day two

--- benches/solution_benchmarks.rs
+++ benches/solution_benchmarks.rs
@@ -1,7 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::hint::black_box;
 
-use aoc_2024::day1;
+use aoc_2024::{day1, day2};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let contents = include_str!("../inputs/day01.txt");
@@ -10,6 +10,12 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
     c.bench_function("Day 01, Part 2", |b| {
         b.iter(|| day1::part2(black_box(contents)))
+    });
+    c.bench_function("Day 02, Part 1", |b| {
+        b.iter(|| day2::part1(black_box(contents)))
+    });
+    c.bench_function("Day 02, Part 2", |b| {
+        b.iter(|| day2::part2(black_box(contents)))
     });
 }
 
---
+++ src/day2.rs
@@ -0,0 +1,112 @@
+// TODO: Try using an array instead of a vector.
+
+struct PartTwoParse<'a> {
+    content: &'a str,
+    idx: usize,
+}
+
+impl<'a> PartTwoParse<'a> {
+    fn parse_next(&mut self, vec: &mut Vec<i64>) -> bool {
+        let mut n = 0i64;
+
+        for idx in self.idx..self.content.len() {
+            let byte = self.content.as_bytes()[idx];
+            if byte.is_ascii_digit() {
+                n *= 10;
+                n += (byte - b'0') as i64
+            } else if byte == b'\n' {
+                vec.push(n);
+                self.idx = idx + 1;
+                return true;
+            } else {
+                vec.push(n);
+                n = 0
+            }
+        }
+        return false;
+    }
+}
+
+impl PartTwoParse<'_> {
+    fn new(content: &str) -> PartTwoParse {
+        PartTwoParse { content, idx: 0 }
+    }
+}
+
+fn is_good_list(list: &[i64]) -> bool {
+    let mut iterator = list.iter().filter(|i| **i != i64::MAX);
+
+    let (mut current, mut next) = (
+        iterator.next().expect("could not retrieve first"),
+        iterator.next().expect("could not retrieve second"),
+    );
+    let initial_delta: i64 = current - next;
+    let negative_delta = if initial_delta == 0 || initial_delta.abs() > 3 {
+        return false;
+    } else {
+        initial_delta.is_negative()
+    };
+
+    for i in iterator {
+        (current, next) = (next, i);
+        let delta = current - next;
+        if (delta == 0) || (delta.is_negative() != negative_delta) || (delta.abs() > 3) {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn part1(contents: &str) -> usize {
+    let mut count = 0usize;
+    let mut list = vec![];
+    let mut parser = PartTwoParse::new(contents);
+
+    while parser.parse_next(&mut list) {
+        if is_good_list(&list) {
+            count += 1
+        }
+        list.clear();
+    }
+    count
+}
+
+
+pub fn part2(contents: &str) -> usize {
+    let mut count = 0usize;
+    let mut list = vec![];
+    let mut parser = PartTwoParse::new(contents);
+
+    while parser.parse_next(&mut list) {
+        if (0..list.len()).any(|idx| {
+            let removed = list[idx];
+            list[idx] = i64::MAX;
+            let to_return = is_good_list(&list);
+            list[idx] = removed;
+            to_return
+        }) {
+            count += 1
+        }
+        list.clear();
+    }
+    count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one_solution() {
+        let contents = include_str!("../inputs/day02.txt");
+        let result = part1(&contents);
+        assert_eq!(result, 432);
+    }
+
+    #[test]
+    fn test_part_two_solution() {
+        let contents = include_str!("../inputs/day02.txt");
+        let result = part2(contents);
+        assert_eq!(result, 488);
+    }
+}
