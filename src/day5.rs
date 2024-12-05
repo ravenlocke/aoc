d2d7cbdb6ff9283db0ab973dbc9a07483cb2f6fa
James Skelton <d.j.skelton1@gmail.com>
2024-12-05 23:01:10 +0000

    Improved solution performance

--- src/day5.rs
+++ src/day5.rs
@@ -1,8 +1,6 @@
 use std::cmp::Ordering;
 
-use itertools::Itertools;
-
-fn parse_radix_10(input: &[u8]) -> usize {
+fn parse_two_digit_radix_10(input: &[u8]) -> usize {
     let mut n = 0;
     for i in input {
         n *= 10;
@@ -13,60 +11,83 @@ fn parse_radix_10(input: &[u8]) -> usize {
     n
 }
 
-pub fn part1(content: &str) -> u64 {
-    let mut rels = [[Ordering::Equal; 128]; 128];
-    for i in 0..1176 {
-        let idx = i * 6;
-        let a = parse_radix_10(&content.as_bytes()[idx..idx + 2]);
-        let b = parse_radix_10(&content.as_bytes()[idx + 3..idx + 5]);
 
-        rels[a][b] = Ordering::Less;
-        rels[b][a] = Ordering::Greater;
+struct LineParser <'a> {
+    idx: usize,
+    content: &'a str
+}
+
+impl LineParser <'_> {
+    fn new(content: &str) -> LineParser {
+        LineParser{content, idx: 0}
     }
 
-    content[1176 * 6 + 1..]
-        .lines()
-        .filter_map(|i| {
-            if i.split(',')
-                .map(|i| parse_radix_10(i.as_bytes()))
-                .tuple_windows()
-                .all(|(a, b)| rels[a][b] == Ordering::Less)
-            {
-                return Some(parse_radix_10(i[i.len() / 2 - 1..i.len() / 2 + 1].as_bytes()) as u64);
+    fn parse_next(&mut self, vec: &mut Vec<usize>) -> bool {
+        while self.idx + 2 < self.content.len() {
+            let x = parse_two_digit_radix_10( &self.content.as_bytes()[self.idx..self.idx+2] );
+            vec.push(x);
+            
+            if self.content.as_bytes()[self.idx + 2] == b'\n' {
+                self.idx += 3;
+                return true
             }
-            None
-        })
-        .sum::<u64>()
+
+            self.idx += 3;
+        }
+
+        false
+    }
+}
+
+fn is_ordered(vec: &Vec<usize>, rels: &[[Ordering; 128]; 128]) -> bool {
+    (0..vec.len()-1).all(|i| rels[vec[i]][vec[i+1]] == Ordering::Less)
+}
+
+pub fn part1(content: &str) -> u64 {
+    let mut rels = [[Ordering::Greater; 128]; 128];
+    for i in 0..1176 {
+        let idx = i * 6;
+        let a = parse_two_digit_radix_10(&content.as_bytes()[idx..idx + 2]);
+        let b = parse_two_digit_radix_10(&content.as_bytes()[idx + 3..idx + 5]);
+
+        rels[a][b] = Ordering::Less;
+    }
+
+    let mut lp = LineParser::new(&content[1_176 * 6 + 1 ..]);
+    let mut vec = Vec::<usize>::with_capacity(23);
+    let mut total = 0;
+    while lp.parse_next(&mut vec) {
+        if is_ordered(&vec, &rels) {
+            total += vec[vec.len() / 2]
+        }
+        vec.clear();
+    }
+    total as u64
 }
 
 pub fn part2(content: &str) -> u64 {
-    let mut rels = [[Ordering::Equal; 128]; 128];
+    let mut rels = [[Ordering::Greater; 128]; 128];
     for i in 0..1176 {
         let idx = i * 6;
-        let a = parse_radix_10(&content.as_bytes()[idx..idx + 2]);
-        let b = parse_radix_10(&content.as_bytes()[idx + 3..idx + 5]);
+        let a = parse_two_digit_radix_10(&content.as_bytes()[idx..idx + 2]);
+        let b = parse_two_digit_radix_10(&content.as_bytes()[idx + 3..idx + 5]);
 
         rels[a][b] = Ordering::Less;
-        rels[b][a] = Ordering::Greater;
     }
 
-    content[1176 * 6 + 1..]
-        .lines()
-        .filter_map(|i| {
-            let mut x: Vec<_> = i.split(',').map(|i| parse_radix_10(i.as_bytes())).collect();
+    let mut lp = LineParser::new(&content[1_176 * 6 + 1 ..]);
+    let mut vec = Vec::<usize>::with_capacity(23);
+    let mut total = 0;
 
-            if x.iter()
-                .tuple_windows()
-                .all(|(a, b)| rels[*a][*b] == Ordering::Less)
-            {
-                return None;
-            }
-
-            x.sort_by(|a, b| rels[*a][*b]);
-
-            Some(x[x.len() / 2] as u64)
-        })
-        .sum::<u64>()
+    while lp.parse_next(&mut vec) {
+        if !is_ordered(&vec, &rels) {
+            vec.sort_unstable_by(|a, b| rels[*a][*b]);
+            total += vec[vec.len() / 2]
+        }
+        vec.clear();
+    }
+    
+    total as u64
 }
 
 #[cfg(test)]
