38069d500859abaa9f2ec795616b0d7aa47b55a7
James Skelton <d.j.skelton1@gmail.com>
2024-12-07 12:33:26 +0000

    Initial solution for day 7

--- benches/solution_benchmarks.rs
+++ benches/solution_benchmarks.rs
@@ -1,7 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use std::hint::black_box;
 
-use aoc_2024::{day1, day2, day3, day4, day5, day6};
+use aoc_2024::{day1, day2, day3, day4, day5, day6, day7};
 
 fn criterion_benchmark(c: &mut Criterion) {
     let contents: &str = include_str!("../inputs/day01.txt");
@@ -50,6 +50,14 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
     c.bench_function("Day 06, Part 2", |b| {
         b.iter(|| day6::part2(black_box(contents)))
+    });
+
+    let contents: &str = include_str!("../inputs/day07.txt");
+    c.bench_function("Day 07, Part 1", |b| {
+        b.iter(|| day7::part1(black_box(contents)))
+    });
+    c.bench_function("Day 07, Part 2", |b| {
+        b.iter(|| day7::part2(black_box(contents)))
     });
 }
 
---
+++ src/day7.rs
@@ -0,0 +1,145 @@
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
+
+#[derive(Debug, Clone)]
+struct ParsedRow {
+    target: usize,
+    numbers: Vec<usize>,
+}
+
+impl ParsedRow {
+    fn maybe_possible_pt1(&self) -> bool {
+        self.numbers[1..]
+            .iter()
+            .fold(self.numbers[0], |sum, i| usize::max(sum + i, sum * i))
+            >= self.target
+    }
+}
+
+struct LineParser<'a> {
+    content: &'a str,
+    idx: usize,
+}
+
+impl LineParser<'_> {
+    fn new(content: &str) -> LineParser {
+        LineParser { content, idx: 0 }
+    }
+}
+
+impl Iterator for LineParser<'_> {
+    type Item = ParsedRow;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut n = 0;
+        let mut target = 0;
+        let mut v = Vec::with_capacity(10);
+
+        while self.idx < self.content.len() {
+            let byte = unsafe { *self.content.as_bytes().get_unchecked(self.idx) };
+            match byte {
+                b'0'..b':' => {
+                    n *= 10;
+                    n += (byte - 48) as usize;
+                    self.idx += 1
+                }
+                b':' => {
+                    target = n;
+                    n = 0;
+                    self.idx += 2;
+                }
+                b' ' => {
+                    v.push(n);
+                    n = 0;
+                    self.idx += 1
+                }
+                b'\n' => {
+                    v.push(n);
+                    self.idx += 1;
+                    return Some(ParsedRow { target, numbers: v });
+                }
+                _ => self.idx += 1,
+            }
+        }
+        None
+    }
+}
+
+fn concatenate(a: usize, b: usize) -> usize {
+    10usize.pow(b.ilog10() + 1) * a + b
+}
+
+unsafe fn is_valid_pt1(target: usize, current: usize, numbers: &[usize]) -> bool {
+    if numbers.len() == 1 {
+        return current + numbers[0] == target || current * numbers[0] == target;
+    } else {
+        return is_valid_pt1(target, current + numbers[0], &numbers[1..])
+            || is_valid_pt1(target, current * numbers[0], &numbers[1..]);
+    }
+}
+
+unsafe fn is_valid_pt2(target: usize, current: usize, numbers: &[usize]) -> bool {
+    if numbers.len() == 1 {
+        return current + numbers.get_unchecked(0) == target
+            || current * numbers.get_unchecked(0) == target
+            || concatenate(current, *numbers.get_unchecked(0)) == target;
+    } else {
+        if current > target {
+            return false;
+        }
+        return is_valid_pt2(target, current + numbers.get_unchecked(0), &numbers[1..])
+            || is_valid_pt2(target, current * numbers.get_unchecked(0), &numbers[1..])
+            || is_valid_pt2(
+                target,
+                concatenate(current, *numbers.get_unchecked(0)),
+                &numbers[1..],
+            );
+    }
+}
+
+pub fn part1(content: &str) -> usize {
+    let mut total = 0;
+
+    for row in LineParser::new(content) {
+        if !row.maybe_possible_pt1() {
+            continue;
+        }
+        if unsafe { is_valid_pt1(row.target, 0, &row.numbers) } {
+            total += row.target
+        }
+    }
+
+    total
+}
+
+pub fn part2(content: &str) -> usize {
+    let rows = LineParser::new(content).collect::<Vec<_>>();
+
+    rows.par_iter()
+        .map(|row| {
+            if unsafe { is_valid_pt2(row.target, 0, &row.numbers) } {
+                row.target
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one_solution() {
+        let contents = include_str!("../inputs/day07.txt");
+        let result = part1(&contents);
+        assert_eq!(result, 20_281_182_715_321);
+    }
+
+    #[test]
+    fn test_part_two_solution() {
+        let contents = include_str!("../inputs/day07.txt");
+        let result = part2(&contents);
+        assert_eq!(result, 159_490_400_628_354);
+    }
+}
